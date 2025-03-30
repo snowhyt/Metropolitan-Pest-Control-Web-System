cfd33b8b0635d8b30ca75cec2f021ae94386d335
unknown <alcantarajomel041@gmail.com>
2025-03-30 20:53:52 +0800

    homePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,7 @@ function HomePage() {
         <div className="text-center text-white font-sans">
           <p>
             Following is the complete list of quality services that are designed
-            to suit the requirements of MPCC’s valued client F
+            to suit the requirements of MPCC's valued client F
           </p>
         </div>
         <div className="flex justify-center items-center space-x-10 mt-10">
@@ -19,41 +19,23 @@ function HomePage() {
         </div>
       </div>
       <div>
-        <div className="text-center text-[30px] text-white font-bold font-sans underline mt-10">
+        <div className="text-center text-[30px] text-blue-400 font-bold font-serif mb-10">
           <h1>Our Company</h1>
         </div>
-        <div>
-          <div className="flex justify-end space-x-10 space-x6 mt-5 border-2 text-center text-white font-sans">
-            <p>
-              Metropolitan Pest Control Corporation utilizes state-of-the-art
-              equipment and the latest technology in the industry.We use only
+        <div className="flex flex-row mx-5 md:mx-10 lg:mx-20 gap-10">
+          <div className="border-2 border-gray-300 p-8 rounded-lg flex-1">
+            <p className="text-gray-700 leading-relaxed">
+              Metropolitan Pest Control Corporation utilizes state-of-the-art
+              equipment and the latest technology in the industry. We use only
               FDA-approved insecticides and chemicals to ensure responsible
-              disposition.​Since 1997, our mission has remained the same—to
-              provide unparalleled service and allow our work to exceed all
-              expectations of professionalism, integrity, and effectiveness.
-              From the smallest and easiest job to the most strenuous, we make
-              sure that we’re not finished until the satisfaction of our
-              customers is met. We aim to finish each project in a timely manner
-              and with the highest level of quality. With a focus on
-              personalized service, competitive rates and customer satisfaction,
-              we’re always striving to meet and exceed industry expectations.
+              disposition. Since 1997,meet and exceed industry expectations.
             </p>
+            <a className="btn bg-blue-400 text-white pt-2 rounded-md mt-6 inline-block hover:bg-blue-500 transition duration-300">
+              Learn More
+            </a>
           </div>
-          <div className="flex justify-end space-x-10 space-x6 mt-5 border-2 text-center text-white font-sans">
-            <p>
-              Metropolitan Pest Control Corporation utilizes state-of-the-art
-              equipment and the latest technology in the industry.We use only
-              FDA-approved insecticides and chemicals to ensure responsible
-              disposition.​Since 1997, our mission has remained the same—to
-              provide unparalleled service and allow our work to exceed all
-              expectations of professionalism, integrity, and effectiveness.
-              From the smallest and easiest job to the most strenuous, we make
-              sure that we’re not finished until the satisfaction of our
-              customers is met. We aim to finish each project in a timely manner
-              and with the highest level of quality. With a focus on
-              personalized service, competitive rates and customer satisfaction,
-              we’re always striving to meet and exceed industry expectations.
-            </p>
+          <div className="border-2 border-gray-300 rounded-lg flex-1">
+            <img src="/logo.png" alt="Company Image" className="w-100 h-50" />
           </div>
         </div>
       </div>
