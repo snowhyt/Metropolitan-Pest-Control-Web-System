1cebf5db8d0ccadb7d0f5c20803b97eb8cfb6464
unknown <alcantarajomel041@gmail.com>
2025-03-30 21:24:15 +0800

    new

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Carousel from "../components/carousel.jsx";
 function HomePage() {
   return (
-
     <div>
       <div className="bg-blue-400 pb-20 pt-20">
         <div className="text-center text-[50px] text-white font-bold font-sans">
@@ -31,6 +30,12 @@ function HomePage() {
               FDA-approved insecticides and chemicals to ensure responsible
               disposition. Since 1997,meet and exceed industry expectations.
             </p>
+            <p className="text-gray-700 leading-relaxed">
+              Metropolitan Pest Control Corporation utilizes state-of-the-art
+              equipment and the latest technology in the industry. We use only
+              FDA-approved insecticides and chemicals to ensure responsible
+              disposition. Since 1997,meet and exceed industry expectations.
+            </p>
             <a className="btn bg-blue-400 text-white pt-2 rounded-md mt-6 inline-block hover:bg-blue-500 transition duration-300">
               Learn More
             </a>
@@ -40,13 +45,6 @@ function HomePage() {
           </div>
         </div>
       </div>
-
-    <div>HomePage
-
-{/* <Carousel/> */}
-
-
-
     </div>
   );
 }
